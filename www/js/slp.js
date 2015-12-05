6bd24035af33ae233029a4d4356cc2ac3f863997
LipingSun <liping.sun@sjsu.edu>
2015-12-05 09:01:50 -0800

    before demo

--- www/js/slp.js
+++ www/js/slp.js
@@ -21,10 +21,27 @@ var slp = {
         })
     },
     register: function () {
-        $.post(host + "api/register", {login: $("#reg_sid").val(),email:$("#reg_email").val(),password:$("#reg_pwd").val()});
-        $.post(host + "api/students", {sid:$("#reg_sid").val(),email:$("#reg_email").val(),lastName:$("#reg_ln").val(),firstName:$("#reg_fn").val()}, function (data) {
-            slp.student = data;
-            slp.loadStudent(data["sid"]);
-        })
+        $.post(host + "/api/authenticate", {username: $("#reg_sid").val(), password: $("#reg_pwd").val()}, function (data) {
+            slp.loadStudent(data["token"].split(':', 1)[0]);
+        });
+
+        //$.ajax({
+        //    type: "POST",
+        //    url: host + "api/register",
+        //    data: JSON.stringify({login: $("#reg_sid").val(),email:$("#reg_email").val(),password:$("#reg_pwd").val()}),
+        //    contentType: "application/json",
+        //    success: function (data) {
+        //        console.log("a:" + data);
+        //    }
+        //});
+
+        //$.post(host + "api/register", {login: $("#reg_sid").val(),email:$("#reg_email").val(),password:$("#reg_pwd").val()}, function (data) {
+        //   console.log("a:" + data);
+        //});
+        //$.post(host + "api/students", {sid:$("#reg_sid").val(),email:$("#reg_email").val(),lastName:$("#reg_ln").val(),firstName:$("#reg_fn").val()}, function (data) {
+        //    console.log("b:" + data);
+        //    slp.student = data;
+        //    slp.loadStudent(data["sid"]);
+        //})
     }
 };
