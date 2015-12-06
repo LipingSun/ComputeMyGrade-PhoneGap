45606ffebf5cff93d9dc3a6fe9fdfc0c4bb2ede6
LipingSun <liping.sun@sjsu.edu>
2015-12-05 19:50:08 -0800

    fix register issue

--- www/js/slp.js
+++ www/js/slp.js
@@ -5,12 +5,15 @@
 
 var slp = {
     host: "http://compute-my-grade.herokuapp.com",
+
     student: null,
+
     login: function () {
         $.post(host + "/api/authenticate", {username: $("#login_sid").val(), password: $("#login_pwd").val()}, function (data) {
             slp.loadStudent(data["token"].split(':', 1)[0]);
         });
     },
+
     loadStudent: function (sid) {
         $.get(host + "/api/students", function (data) {
             slp.student = $.grep(data, function (student) {
@@ -20,31 +23,33 @@ var slp = {
             location.href = "#home_page";
         })
     },
+
     register: function () {
-        $.post(host + "/api/authenticate", {username: $("#reg_sid").val(), password: $("#reg_pwd").val()}, function (data) {
-            slp.loadStudent(data["token"].split(':', 1)[0]);
+        $.ajax({
+            type: "POST",
+            url: host + "/api/register",
+            data: JSON.stringify({login: $("#reg_sid").val(),email:$("#reg_email").val(),password:$("#reg_pwd").val()}),
+            contentType: "application/json",
+            success: function (data) {
+                console.log("data:" + data);
+                $.ajax({
+                    type: "POST",
+                    url: host + "/api/students",
+                    data: JSON.stringify({sid:$("#reg_sid").val(),email:$("#reg_email").val(),lastName:$("#reg_ln").val(),firstName:$("#reg_fn").val(),phone:$("#reg_phone").val()}),
+                    contentType: "application/json",
+                    success: function (data) {
+                        console.log("data:" + data);
+                        slp.student = data;
+                        slp.loadStudent(data["sid"]);
+                    },
+                    error: function (err) {
+                        console.log("error:" + err)
+                    }
+                });
+            },
+            error: function (err) {
+                console.log("error:" + err)
+            }
         });
-
-        //$.ajax({
-        //    type: "POST",
-        //    url: host + "api/students",
-        //    data: JSON.stringify({sid:$("#reg_sid").val(),email:$("#reg_email").val(),lastName:$("#reg_ln").val(),firstName:$("#reg_fn").val()}),
-        //    contentType: "application/json",
-        //    success: function (data) {
-        //        console.log("a:" + data);
-        //    },
-        //    error: function (err) {
-        //        console.log("error:" + err)
-        //    }
-        //});
-
-        //$.post(host + "api/register", {login: $("#reg_sid").val(),email:$("#reg_email").val(),password:$("#reg_pwd").val()}, function (data) {
-        //   console.log("a:" + data);
-        //});
-        //$.post(host + "api/students", {sid:$("#reg_sid").val(),email:$("#reg_email").val(),lastName:$("#reg_ln").val(),firstName:$("#reg_fn").val()}, function (data) {
-        //    console.log("b:" + data);
-        //    slp.student = data;
-        //    slp.loadStudent(data["sid"]);
-        //})
     }
 };
